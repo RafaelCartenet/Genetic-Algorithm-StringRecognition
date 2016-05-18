f7896861a36b99d11ccca84edc5a006a5317d003
starkhorndev <starkthehunt@gmail.com>
2016-05-18 14:55:54 +0200

    modifications finales

--- GAString/include/GA_parameters.h
+++ GAString/include/GA_parameters.h
@@ -2,24 +2,22 @@
 #define GA_PARAMETERS_H
 
 
-#define target string("Hello world ! Ici Killian Jaubert, Maxime Aberton et Rafael Cartenet.")
+#define target string("Hello world ! Ici Maxime Aberton, Killian Jaubert et Rafael Cartenet. Quel temps fait-il ?")
 
 #define SIZE target.size()
 #define RANDOM (float)(rand()%100+1)/100
-#define NBGENE 10000
+#define NBGENE 30000
 
 //Paramètres du GA
 
 #define ELITRATE 0.20
-#define CROSSOVERRATE 0.65
-#define NBGENOME 50
-// On pourrait éventuellement mettre (SIZE%2==1?:SIZE) pour le nb de génomes.
-// Pour des phrases plus longues, on souhaiterait avoir plus de génomes afin de tomber plus facilement sur les lettres qui nous manquent
-#define DELTAFITNESS 1
-#define COEFFMUTERATE 1.008
-#define MUTERATEMAX 0.05
+#define CROSSOVERRATE 0.6
+#define NBGENOME (SIZE%2==1?SIZE+1:SIZE)
 
-//On définit MUTERATE ainsi afin d'avoir la possibilité de le faire varier dans l'algo.
+#define MUTERATEMAX 0.04
+#define COEFFMUTERATE 1.008
+#define DELTAFITNESS 1
+
 static double MUTERATE=0.01;
 
 #endif
